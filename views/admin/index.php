6625df1437aecf6e47801c392743c3efc307480e
Andrey Shipul work <sh00ter@tut.by>
2017-08-10 15:49:00 +0300

    add

---
+++ views/admin/index.php
@@ -0,0 +1,40 @@
+<ul id="nav" class="nav nav-tabs">
+    <li><a href="/admin/editNews">Новости</a></li>
+    <li><a href="/admin/editMenu">Меню</a></li>
+  <li><a href="/stat/">Статистика</a></li>
+</ul>
+ 
+
+<div id="ajax-content">This is default text, which will be replaced</div>
+
+<!--загружаем здесь скрипт датапикера чтобы не было дубликатов после аякса , при дубликатов датапикер не работает-->
+
+<script src="/public/js/jquery-ui.datepicker.js"></script>
+
+
+
+<script>
+$(document).ready(function() {
+    $("#nav li a").click(function() {
+        $("#nav li").removeClass('active');
+        $(this).parent().addClass('active');        
+        $.ajax({url: this.href,cache:false,success: function(html) {
+            $("#ajax-content").empty().append(html); 
+            
+            }
+    });
+    return false;
+    });
+    
+    
+     $("#ajax-content").empty().append("<h1>Загрузка ....</h1>");
+    $.ajax({ url: '/admin/editNews',cache:false, success: function(html) {
+            $('#nav li a[href="/admin/editNews"]').parent().addClass('active');  // start page News add active class to li
+            $("#ajax-content").empty().append(html);
+            
+    }
+    });
+    
+    
+   
+});</script>
---
+++ views/stat/test.php
@@ -0,0 +1,8 @@
+
+<link rel="stylesheet" href="/public/js/test/jodit.min.css">
+<script src="/public/js/test/jodit.min.js"></script>
+
+
+<textarea id="editor" name="editor"></textarea>
+<script>var editor = new Jodit('#editor');</script>
+
